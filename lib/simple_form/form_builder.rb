409dce9e51b536a72524756c54f1aecb979de836
Gaston Morixe <imton@users.noreply.github.com>
2015-09-06 03:24:37 -0300

    Update form_builder.rb

--- lib/simple_form/form_builder.rb
+++ lib/simple_form/form_builder.rb
@@ -473,7 +473,8 @@ module SimpleForm
     def build_association_attribute(reflection, association, options)
       case reflection.macro
       when :belongs_to
-        (reflection.respond_to?(:options) && reflection.options[:foreign_key]) || :"#{reflection.name}_id"
+        public_id = reflection.respond_to?(:token) ? :#{reflection.name}[#{token}] :  :"#{reflection.name}_#{id}"
+        (reflection.respond_to?(:options) && reflection.options[:foreign_key]) || public_id
       when :has_one
         raise ArgumentError, ":has_one associations are not supported by f.association"
       else
